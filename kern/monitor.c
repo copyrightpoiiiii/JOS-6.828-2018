42366d971dd0bfaa8ece46f0c8fddb787d857e8c
copyright <barcelonakeyfc@qq.com>
2020-02-27 00:12:36 +0800

    lab1 finish

--- kern/monitor.c
+++ kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display information about the stack", mon_backtrace },
 };
 
 /***** Implementations of basic kernel monitor commands *****/
@@ -58,10 +59,23 @@ int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	// Your code here.
+
+	uint32_t *ebp = (uint32_t*)read_ebp();
+	cprintf("Stack backtrace:\n");
+	while (ebp != NULL){
+		uint32_t eip = *(ebp+1);
+		cprintf("  ebp %08x  eip %08x  args %08x %08x %08x %08x %08x\n",ebp,eip,*(ebp+2),*(ebp+3),*(ebp+4),*(ebp+5),*(ebp+6));
+		struct Eipdebuginfo info;
+		if(debuginfo_eip(eip,&info)!=-1){
+			cprintf("         %s:%d: %.*s+%d\n",info.eip_file,info.eip_line,info.eip_fn_namelen,info.eip_fn_name,eip-info.eip_fn_addr);
+		}
+		ebp=(uint32_t *)*(ebp);
+	}
+
 	return 0;
 }
 
-
+ 
 
 /***** Kernel monitor command interpreter *****/
 
